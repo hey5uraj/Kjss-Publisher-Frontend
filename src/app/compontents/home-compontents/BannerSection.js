50c4bcc97b315031ff9c71d9815740341521ff5c
hey5uraj <surajjadhav4621@gmail.com>
2026-06-29 22:22:41 +0530

    initial

---
+++ src/app/compontents/home-compontents/BannerSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BannerSection() {
+  return (
+    <div>
+      <section className='bg-[url("https://pinnacle-khaki.vercel.app/mediaFiles/Gallery/sample-flat01/Play-Zone.webp")] 
+      bg-cover bg-center h-screen w-[100%]'></section>
+    </div>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "./compontents/common/Footer";
+import Header from "./compontents/common/Header";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +13,17 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        <Header />
+
+        {children}
+
+        <Footer />
+
+      </body>
     </html>
   );
 }
